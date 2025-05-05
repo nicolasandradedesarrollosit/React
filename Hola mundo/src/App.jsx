f4eb22e32f2e1a2a0ddb8889e30087d9becf4f0f
nicolasandradedesarrollosit <nicolasandradedesarrollos@gmail.com>
2025-05-04 22:34:37 -0300

    finalizacion de la primera practica midulive

--- Hola mundo/src/App.jsx
+++ Hola mundo/src/App.jsx
@@ -1,39 +1,27 @@
 import './index.css';
 import './App.css';
-import { TwitterFollowCard } from './Twitter-Follow-Card.jsx'
-import { useState } from 'react';
-
+import { TwitterFollowCard } from './Twitter-Follow-Card.jsx';
 
 export function App () {
-    const [name,setName] = useState(false)
+  const leomessi = {
+    username: 'lionelmessi',
+    initialIsFollowing: true
+  };
+  
+  const cobratate = {
+    username: 'cobratate',
+    initialIsFollowing: false
+  };
 
-    const text = name ? 'Andresito' : 'Andres';
+  return (
+    <div className="App">
+      <TwitterFollowCard {...leomessi}>
+        <strong>Leo Messi</strong>
+      </TwitterFollowCard>
 
-    const nameChange = () => {
-        setName(!name)
-    }
-
-    const leomessi = {
-        username: 'lionelmessi',
-        isFollowing: true
-    }
-    const cobratate = {
-        username: 'cobratate',
-        isFollowing: false
-    }
-
-    return (
-        <div className="App">
-            <TwitterFollowCard username={text} >
-                <strong>Leo Messi</strong>
-            </TwitterFollowCard>
-
-            <TwitterFollowCard {...cobratate} >
-                <strong>Andrew Tate</strong>
-            </TwitterFollowCard>
-            <button onClick={nameChange}>Cambio nombre</button>
-        </div>
-    )
-
-    
+      <TwitterFollowCard {...cobratate}>
+        <strong>Andrew Tate</strong>
+      </TwitterFollowCard>
+    </div>
+  );
 }
--- Hola mundo/src/Twitter-Follow-Card.jsx
+++ Hola mundo/src/Twitter-Follow-Card.jsx
@@ -1,35 +1,36 @@
 import { useState } from "react";
-export function TwitterFollowCard ({children, username}) {
-  const [isFollowing, setIsFollowing] = useState(false);
+import './App.css';
+
+export function TwitterFollowCard ({children, username, initialIsFollowing}) {
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing);
 
   const text = isFollowing ? 'Siguiendo' : 'Seguir';
-
   const buttonClassName = isFollowing ? 'btn-follow is-following' : 'btn-follow';
-
   const imageSrc = `https://unavatar.io/${username}`;
 
   const followClick = () => {
     setIsFollowing(!isFollowing);
   }
+
   return (
-      <article className="card-follow">
-        <header className="card-follow-header">
-          <img className="avatar-follow" src= {imageSrc} alt="foto perfil" />
-          <div className="card-follow-header-div">
-            <strong className="card-follow-header-name">
-            {children}
-            </strong>
-            <span className="card-follow-header-info">
-              <strong>@{username}</strong>
-            </span>
-          </div>
-        </header>
-        <aside>
-          <button className={buttonClassName} onClick={followClick}>
-            {text}
-          </button>
-        </aside>
-      </article>
-    );
+    <article className="card-follow">
+      <header className="card-follow-header">
+        <img className="avatar-follow" src={imageSrc} alt="foto perfil" />
+        <div className="card-follow-header-div">
+          <strong className="card-follow-header-name">{children}</strong>
+          <span className="card-follow-header-info">
+            <strong>@{username}</strong>
+          </span>
+        </div>
+      </header>
+      <aside>
+        <button className={buttonClassName} onClick={followClick}>
+          <span className="btn-text">{text}</span>
+          <span className="stop-follow">Dejar de seguir</span>
+        </button>
+      </aside>
+    </article>
+  );
 }
+
   
